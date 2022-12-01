ce6bb0866adf48f9481f39e34e8a0a4adb6a2bc9
georgkiim <kimkim99975@gmail.com>
2022-12-01 18:54:45 +0300

    all

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -9,15 +9,13 @@ type PropsType = {
     setFilteredTasks: (newFilter: FilterType) => void
     filter: FilterType
 }
-
-
 const ToDoList = (props: PropsType) => {
     const [task, setTask] = useState('')
     const [error, setError] = useState('')
     const tasksList = props.tasks.length !== 0
         ? props.tasks.map(t => {
             return (
-                <li key={t.id} className={t.isDone===true?'isDone':'tit'} >
+                <li key={t.id} className={t.isDone === true ? 'isDone' : 'tit'}>
                     {t.task}
                     <input
                         onChange={(e) => props.changeCheck(t.id, e.currentTarget.checked)}
@@ -27,7 +25,6 @@ const ToDoList = (props: PropsType) => {
 
             )
         }) : 'Список задач пуст!:)'
-
     const addNewTask = () => {
         if (task.trim() !== '') {
             props.addTask(task)
@@ -37,50 +34,36 @@ const ToDoList = (props: PropsType) => {
             setError("Введите задачу!")
         }
     }
-
     const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
             addNewTask()
         }
     }
-
-
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setTask(e.currentTarget.value)
+        setError("")
+    }
+    const setFilterClass = (filter: FilterType) => props.filter === filter ? 'button-active' : ''
+    const OnClickSetFilter = (newFilter: FilterType) => () => props.setFilteredTasks(newFilter)
     return (
         <div className="todolistDone">
             <h2 className='header'> Список задач</h2>
-           <div className='poop'>
-            <input
-                value={task}
-                onChange={(e) => {
-                    setTask(e.currentTarget.value)
-                    setError("")
-                }}
-                onKeyDown={(e) => onEnter(e)}
-                className={error ? 'error' : 'inputciv'}
-            />
-            <button className='addButton' onClick={() => addNewTask()}>Добавить задачу</button>
-            {error && <div className='error-message'>{error}</div>}
-           </div>
-            <ul>
-                {tasksList}
-            </ul>
-            <button className={props.filter === 'all' ? 'button-active' : ''}
-                    onClick={() => props.setFilteredTasks('all')}>
+            <div className='poop'>
+                <input value={task} onChange={onChange} onKeyDown={onEnter} className={error ? 'error' : 'inputciv'}/>
+                <button className='addButton' onClick={addNewTask}>Добавить задачу</button>
+                {error && <div className='error-message'>{error}</div>}
+            </div>
+            <ul>{tasksList}</ul>
+            <button className={setFilterClass("all")} onClick={OnClickSetFilter('all')}>
                 Все здачи
             </button>
-            <button
-                className={props.filter === 'active' ? 'button-active' : ''}
-                onClick={() => props.setFilteredTasks('active')}>
+            <button className={setFilterClass('active')} onClick={OnClickSetFilter('active')}>
                 Надо делать!
             </button>
-            <button
-                className={props.filter === 'completed' ? 'button-active' : ''}
-                onClick={() => props.setFilteredTasks('completed')}>
+            <button className={setFilterClass('completed')} onClick={OnClickSetFilter('completed')}>
                 Завершенные
             </button>
         </div>
     )
 }
-
-
 export default ToDoList
